41c7fdd45b993d5d677cfb08c61d3ed8422be3f3
rubens <rubens.c17@gmail.com>
2021-08-14 19:34:42 -0300

    Retirado log

--- src/components/MovieRow/index.jsx
+++ src/components/MovieRow/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './styles.css'
 
 const MovieRow = ({ title, items }) => {
-  console.log(items.results);
   return (
     <div className="movieRow">
       <h2>{title}</h2>
