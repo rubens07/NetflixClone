662982ebe3983e745a528fe4c171301593949d32
rubens <rubens.c17@gmail.com>
2021-08-13 21:33:34 -0300

    Importação dos dados via API

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React, { useEffect } from 'react'
+import HomeList from './database/tmdb';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const dados = HomeList.getHomeList();
+    console.log(dados);
+  }, []);
+
   return (
     <div className="App">
      Hello World!
