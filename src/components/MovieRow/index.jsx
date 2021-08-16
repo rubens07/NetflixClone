3e0e622a7d6c28587dc6ac3b7706d22429996b95
rubens <rubens.c17@gmail.com>
2021-08-16 20:50:21 -0300

    Criado efeito e estilo para percorrer a lista de filmes

--- src/components/MovieRow/index.jsx
+++ src/components/MovieRow/index.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import './styles.css'
 
 const MovieRow = ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(0);
+
+  const handleLeftArrow = () => {
+    // percorre metade da tela do dispositivo
+    let x = scrollX + Math.round(window.innerWidth / 2);
+    if (x > 0){
+      x = 0;
+    }
+    setScrollX(x);
+  }
+
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2);
+    // tamanho total da lista de filmes
+    const listW = items.results.length * 175;
+    const dif = window.innerWidth - listW;
+    if (x < dif) {
+      x = dif - 60; // 30px de padding na direita e 30px na esquerda
+    }
+    setScrollX(x);
+  }
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
+      <div className="movieRow-left">
+        <NavigateBeforeIcon
+          style={{fontSize: 50}}
+          onClick={handleLeftArrow}
+        />
+      </div>
+      <div className="movieRow-right">
+        <NavigateNextIcon
+          style={{fontSize: 50}}
+          onClick={handleRightArrow}
+        />
+      </div>
       <div className="movieRow-listarea">
-        <div className="movieRow-list">
+        <div className="movieRow-list" style={{ marginLeft: scrollX }}>
           {items.results.length > 0 && items.results.map((movie, index) => {
             if (movie.poster_path) {
               return (
@@ -18,10 +54,10 @@ const MovieRow = ({ title, items }) => {
                   />
                 </div>
               );
-            }
+            } else return null;
           })
-          }
-        </div>
+        }
+      </div>
       </div>
     </div>
   );
